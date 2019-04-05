c1dff1e57df6347747370df482182bc06e5d77fc
Mariia <mariiapopovych@gmail.com>
2019-04-05 11:23:31 -0700

    replace number 1 with Bob

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,57 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
-    var number = $("input#userInput").val();
-    var count1 = 0;
-    var count2 = 0;
-    var count3 = 0;
-    for (var i = 0; i <= number.length; i++) {
+    var number = parseInt($("input#userInput").val());
 
-      if (number[i] === "1") {
-        count1 += 1;
+
+    for (i = 0; i <= number; i++) {
+      var number1 = i.toString();
+      for (j = 0; j < number1.length; j++) {
+        if (number1[j] === "1") {
+          number1 = number1.replace(number1[j], "Bob");
+
+
+        }
+
       }
-      if (number[i] === "2") {
-        count2 += 1;
-      }
-      if (number[i] === "3") {
-        count3 += 1;
-      }
+      console.log(number1);
     }
 
-
-    if (count3 > 0) {
-      console.log("Sorry");
-    } else if (count2 > 0) {
-      console.log("Bob");
-    } else if (count1 > 0) {
-      console.log("Beep");
-    } else {
-      console.log(number);
-    }
-    console.log(count1, count2, count3);
-
-
-
-
+    // var count1 = 0;
+    // var count2 = 0;
+    // var count3 = 0;
+    // for (var i = 0; i <= number.length; i++) {
+    //
+    //   if (number[i] === "1") {
+    //     count1 += 1;
+    //   }
+    //   if (number[i] === "2") {
+    //     count2 += 1;
+    //   }
+    //   if (number[i] === "3") {
+    //     count3 += 1;
+    //   }
+    // }
+    //
+    //
+    // if (count3 > 0) {
+    //   console.log("Sorry");
+    // } else if (count2 > 0) {
+    //   console.log("Bob");
+    // } else if (count1 > 0) {
+    //   console.log("Beep");
+    // } else {
+    //   console.log(number);
+    // }
+    // console.log(count1, count2, count3);
+    //
+    //
+    // if number > 10 {
+    //   else {
+    //
+    //   }
+    // }
+    //
 
 
   });
