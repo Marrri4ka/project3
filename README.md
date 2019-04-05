dc0fc3898171ea6e98882b3fb97910152f5f6a95
Mariia <mariiapopovych@gmail.com>
2019-04-05 12:19:30 -0700

    add REAME information and change js logic, so wher the user enter 12, user will see Bob, or entering 32, user will see /'I'm sorry..'/

---
+++ README.md
@@ -0,0 +1,42 @@
+# _Beep Boop_
+
+#### _Information about different courses in Epicodus bootcamp, 04.05.2019_
+
+#### By _**Mariia Stashuk**_
+
+## Description
+
+_Web application that takes a number from a user and returns a range of numbers from 0 to the user inputted number with the following exceptions:
+
+Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
+Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
+Numbers that contain a 3: all digits are replaced (all digits) with "I'm sorry, Dave. I'm afraid I can't do that."_
+
+
+## Setup/Installation Requirements
+
+_To run this project,install it locally:_
+
+* Clone this repository https://github.com/Marrri4ka/project3.git
+* _Open index.html with your browser of choice or open https://marrri4ka.github.io/project3/_
+
+## Known Bugs
+
+Some links are not operational currently
+
+## Support and contact details
+
+If you have any questions or problems, please contact Mariia (mariiapopovych@gmail.com)
+
+## Technologies Used
+
+_HTML_
+_CSS_
+_JavaScript_
+_JQuery_
+
+### License
+
+*License under MIT*
+
+Copyright (c) 2019 **_Mariia Stashuk_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,47 @@
+//UI logic
+
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#userInput").val());
+    var res = "";
 
+    if (isNaN(number) || number === "") {
+      alert("Please enter a number!")
+    } else {
+      for (i = 0; i <= number; i++) {
+        var number1 = i.toString();
 
-    for (i = 0; i <= number; i++) {
-      var number1 = i.toString();
-      for (j = 0; j < number1.length; j++) {
-        if (number1[j] === "1") {
-          number1 = number1.replace(number1[j], "Bob");
+        var count1 = 0;
+        var count2 = 0;
+        var count3 = 0;
+        for (var k = 0; k <= number1.length; k++) {
 
-
+          if (number1[k] === "1") {
+            count1 += 1;
+          }
+          if (number1[k] === "2") {
+            count2 += 1;
+          }
+          if (number1[k] === "3") {
+            count3 += 1;
+          }
         }
 
+
+        if (count3 > 0) {
+          res = "Sorry";
+        } else if (count2 > 0) {
+          res = "Bob";
+        } else if (count1 > 0) {
+          res = "Beep";
+        } else {
+          res = number1;
+        }
+        $("ul").append("<li>" + res + "</li>");
+
       }
-      console.log(number1);
     }
-
-    // var count1 = 0;
-    // var count2 = 0;
-    // var count3 = 0;
-    // for (var i = 0; i <= number.length; i++) {
-    //
-    //   if (number[i] === "1") {
-    //     count1 += 1;
-    //   }
-    //   if (number[i] === "2") {
-    //     count2 += 1;
-    //   }
-    //   if (number[i] === "3") {
-    //     count3 += 1;
-    //   }
-    // }
-    //
-    //
-    // if (count3 > 0) {
-    //   console.log("Sorry");
-    // } else if (count2 > 0) {
-    //   console.log("Bob");
-    // } else if (count1 > 0) {
-    //   console.log("Beep");
-    // } else {
-    //   console.log(number);
-    // }
-    // console.log(count1, count2, count3);
-    //
-    //
-    // if number > 10 {
-    //   else {
-    //
-    //   }
-    // }
-    //
-
 
   });
 
